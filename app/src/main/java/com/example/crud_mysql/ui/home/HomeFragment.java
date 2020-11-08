b87e8e662de412fa52c043cc1b0671403d62ecea
kennWNK97 <castro21.kc@gmail.com>
2020-11-08 10:20:50 -0600

    Subir el proyecto

---
+++ app/src/main/java/com/example/crud_mysql/ui/home/HomeFragment.java
@@ -0,0 +1,122 @@
+package com.example.crud_mysql.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.example.crud_mysql.MySingleton;
+import com.example.crud_mysql.R;
+
+import org.json.JSONObject;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+    private TextView textView;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                ViewModelProviders.of(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        textView = root.findViewById(R.id.text_home);
+
+        final Button btn1 = root.findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pruebaVolley();
+
+                //peticionGson();
+
+                //recibirJson();
+
+
+            }
+        });
+
+
+       /* homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+        return root;
+    }
+
+    //METODO DE PRUEBA
+    private void pruebaVolley() {
+        String url = "http://httpbin.org/html";
+        // http://192.168.1.12/service2020/
+        //Request a String response
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response.substring(0, 16));
+                textView.setText(response.substring(0, 16));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Error  handling
+
+                System.out.println("Something went wrong!");
+                Toast.makeText(getContext(), "Sin conexión a Internet", Toast.LENGTH_SHORT).show();
+                error.printStackTrace();
+            }
+        });
+
+        MySingleton.getInstance(getContext()).addToRequestQueue(stringRequest);
+
+    }
+
+    private void peticionGson(){
+
+        //  String url ="http://192.168.1.12/service2020/json1.php";
+        String url = Setting_VAR.URL_PRUEBA;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, (String) null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                textView.setText("Response" + response.toString());
+                Toast.makeText(getContext(), "" +response.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        MySingleton.getInstance(getContext()).addToRequestQueue(jsonObjectRequest);
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/crud_mysql/ui/home/Setting_VAR.java
@@ -0,0 +1,20 @@
+package com.example.crud_mysql.ui.home;
+
+
+
+
+
+public class Setting_VAR {
+    public static final  String TAG = "InicioActivity";
+
+
+    public static final String mensajex="";
+    public static final String URL_PRUEBA="http://192.168.1.13//service2020/json1.php";
+    public static final String URL_guardar_categoria="http://192.168.1.13//service2020/guardar_categoria.php";
+    public static final String URL_registrar_productos ="http://192.168.1.13//service2020/guardar_categoria.php";
+    public static final String URL_consultaAllCategorias ="http://192.168.1.13/service2020/buscar_Categorias.php";
+
+
+}
+
+
